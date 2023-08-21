8ff424df8c450b08a661ac5d81872f29ffeb8b73
renan-augusto <renan_acm1992@hotmail.com>
2023-08-21 07:24:38 -0300

    calculadora fixes

--- calculadora.rb
+++ calculadora.rb
@@ -7,24 +7,32 @@ loop do
     puts '3-Multiplicacao'
     puts '4-Divisao'
     puts 'Ou pressione qualquer outra tecla para sair'
-
     option = gets.chomp.to_f
+
+    break if option > 4 || option < 1
     puts 'Digite o primeiro numero'
     num1 = gets.chomp.to_f
     puts 'Digite o segundo numero'
     num2 = gets.chomp.to_f
-    result = num1 + num2
-    if option == 1
+
+    case option
+    when 1
         result = num1 + num2
         puts "O resultado da adicao e #{result}"
-    elsif option == 2
+    when 2
         result = num1 - num2
-    elsif option == 3
+        puts "O resultado da subtracao e #{result}"
+    when 3
         result =  num1 * num2
-    elsif option == 4
-        result = num1 / num2  
-    elsif option > 4 || option < 1
-        break if option >4 || option < 1
+        puts "O resultado da multiplicacao e #{result}"
+    when 4
+        if num2 != 0
+            result = num1 / num2 
+            puts "O resultado da divisao e #{result}" 
+        else
+            puts "Nao existe divisao por 0" 
+            result = 'Erro'
+        end
     else
         resultado = 'Opcao invalida' 
     end
