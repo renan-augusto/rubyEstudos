9d4304610b951c13bc221d61aab25a6fd4eb09b5
renan-augusto <renan_acm1992@hotmail.com>
2023-09-19 17:46:29 -0300

    atualziacao estudos

---
+++ iteracao.rb
@@ -0,0 +1,7 @@
+linguagens = ['Ruby', 'go', 'C++', 'JavaScript']
+count = 1 
+for linguagem in linguagens
+    puts linguagem
+    puts count
+    count += 1
+end
---
+++ times.rb
@@ -0,0 +1,14 @@
+contador = 0
+# #exemplo 1 puts 
+# 6.times {puts "ooi "}
+# #exemplo 1 print
+# 6.times {print "ooi "}
+
+#exemplo 2 puts
+# 100.times do
+#     puts "oi"
+# end
+#exemplo 3 contador puts 
+21.times do |contador|
+    puts "#{contador}"
+end
